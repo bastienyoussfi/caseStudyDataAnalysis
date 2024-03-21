8f1cb8620ad92aa2391f1c30a8c6df7c598080bb
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2024-03-21 14:44:15 +0900

    Nearly finished

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Reading the different tables
+# Let's first read the different tables
 rentals = pd.read_csv("Dataset/rentals_table.csv")
 customers = pd.read_csv("Dataset/customers_table.csv")
 trailers = pd.read_csv("Dataset/trailers_table.csv")
 
 # ----------------------------------------------------------------------------- #
 
-# Query using groupby to count trailers for each make
+# Here is the query using groupby to count trailers for each make
 trailer_count_per_company = trailers.groupby('make').size().reset_index(name='available_trailer_count')
 
 # print(trailer_count_per_company)
@@ -87,7 +87,51 @@ rentals['rental_period'] = rentals['return_date'] - rentals['rental_date']
 # Sum the rental periods to get the total rental duration
 total_rental_duration_per_trailer = rentals.groupby('trailer_id')['rental_period'].sum()
 
-print("Total Rental Duration:", total_rental_duration_per_trailer)
+# print("Total Rental Duration:", total_rental_duration_per_trailer)
 
 # ----------------------------------------------------------------------------- #
 
+# Merge rental data with customer data based on 'customer_id'
+merge = pd.merge(rentals, customers, on='customer_id', how='inner')
+
+# Group by customer industry and calculate the count of rentals for each industry
+rental_demand_trends = merge.groupby('industry').size().reset_index(name='rental_count')
+
+# print(rental_demand_trends)
+
+# ----------------------------------------------------------------------------- #
+
+# For this question we'll consider the first 10 trailers
+
+# ----------------------------------------------------------------------------- #
+
+# Convert 'start_time' to datetime objects and extract month and year
+rentals['start_time'] = pd.to_datetime(rentals['rental_date'])
+rentals['rental_month'] = rentals['rental_date'].dt.month
+rentals['rental_year'] = rentals['rental_date'].dt.year
+
+# Let's then merge the rentals data with the customers data in order to have both time and locations
+merge = pd.merge(rentals, customers, on='customer_id', how='inner')
+
+# Let's then group by location
+demand_based_on_location = merge.groupby(['country', 'rental_year', 'rental_month']).size().reset_index(name='Rental count')
+
+# print(demand_based_on_location.sort_values(by=['rental_year', 'rental_month']))
+
+# ----------------------------------------------------------------------------- #
+
+# Let's merge the customers and rentals data to see which customers have the most profitable rentals
+merge = pd.merge(rentals, customers, on="customer_id", how="inner")
+
+# Let's then group by industry and sum both the rentals duration and the total price
+total_price = merge.groupby(['industry'])['rental_price_eur'].sum().reset_index(name='Total price')
+total_rental_duration_per_industry = merge.groupby(['industry'])['duration'].sum().reset_index(name='Total rental duration')
+
+# Then, let's check how profitable are each industry's rentals by diving the two columns
+prof = pd.DataFrame()
+prof['industry'] = total_price['industry']
+prof['profitability'] = total_price['Total price'].div(total_rental_duration_per_industry['Total rental duration'])
+
+print(prof)
+
+# We see that the Manufacturing industry has the most profitable rentals as their ratio is the lowest
